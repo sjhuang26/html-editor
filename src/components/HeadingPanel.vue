5a4f59bbc42690c0f098f664b53aa241c3d0a6b3
Zifeng "Enrique" Su <suzihitaki@gmail.com>
2018-10-17 05:18:57 +0900

    Improved header
    Improved header with picture and bootstrap

--- src/components/HeadingPanel.vue
+++ src/components/HeadingPanel.vue
@@ -1,19 +1,37 @@
 <!-- a panel that contains the heading -->
 <template>
-  <div>
-    <h1>Easy HTML Editor</h1>
-    <p>Write code, learn HTML</p>
-  </div>
+  <nav class="navbar navbar-expand-md">
+
+    <div class="navbar-brand">
+      <img style="max-width:275px; max-height:67px; margin-top: -7px;" src="../img/Brand.png" width="1099"
+           height="266" alt="Brand" class="img-responsive">
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          A project by Suhao Jeffrey Huang
+        </li>
+    </ul>
+    </div>
+  </nav>
+
 </template>
 
 <script>
-export default {
-  name: 'HeadingPanel',
+  export default {
+    name: 'HeadingPanel',
     props: {
-        name: {
-            title: 'Heading',
-            description: "TODO"
-        }
+      name: {
+        title: 'Heading',
+        description: "TODO"
+      }
     },
-};
+  };
 </script>
+
+<style scoped>
+  .navbar-expand-md {
+    background-color: #FFFFFF;
+    border-color: #FFFFFF;
+  }
+</style>
