9c6b4a3169c8ec2990ef42dc5e3f5f6a165bbfdf
Zifeng "Enrique" Su <suzihitaki@gmail.com>
2018-12-05 05:50:34 +0900

    Add additional code examples for each tag.

--- src/js/tutorial.js
+++ src/js/tutorial.js
@@ -25,6 +25,10 @@ export const tutorial = {
                 {
                     description: `An h1 tag followed by two p tags. Try changing the words inside the tags.`,
                     code: `<h1>Hi</h1>\n<p>Hi!</p>\n<p>Hi again.</p>`
+                },
+                {
+                    description: `A comment. It won't show up on the web page.`,
+                    code: `<p>Hi</p>\n<!-- Your comments here --><p>Another hi</p>`
                 }
             ]
         },
@@ -33,8 +37,20 @@ export const tutorial = {
             lessonMarkdownContent: lessonContent_nestingTags,
             examples: [
                 {
-                    description: 'An "i" tag inside a "h1" tag.',
+                    description: `An "i" tag inside a "h1" tag. It set inside text to italic.`,
                     code: `<h1><i>Hi!</i></h1>`
+                },
+                {
+                    description: `A "br" tag inside a "h2" tag. It breaks a single line.`,
+                    code: `<h2>Separates<br>lines</h2>`
+                },
+                {
+                    description: `A "code" tag inside a "h3" tag. It defines a piece of computer code.`,
+                    code: `<h3>A line of text<br>and<br><code>a line of code</code></h3>`
+                },
+                {
+                    description: `A "u" tag inside a "h4" tag. It set inside text underlined.`,
+                    code: `<h4><u>Underlined text</u></h4>`
                 }
             ]
         },
@@ -71,6 +87,18 @@ export const tutorial = {
                 {
                     description: 'What about both height and width 200px?',
                     code: `<button height="200px" width="200px">Hello</button>`
+                },
+                {
+                    description: 'A hyperlink to google.com.',
+                    code: `<a href="https://www.google.com">\nThis is a link</a>\n<a>This is not a link</a> `
+                },
+                {
+                    description:'A red colored paragraph.',
+                    code: `<p style="color:red">\nI am a red paragraph.</p>`
+                },
+                {
+                    description:'A paragraph with a tooltip. Mouse over the paragraph to display the tooltip.',
+                    code: `<p title="This is a tooltip">\nThis is a paragraph with a tooltip.</p>`
                 }
             ]
         }
